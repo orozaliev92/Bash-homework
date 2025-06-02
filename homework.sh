3231d39ec74e7a8d9d11af5edc4755ad248aed11
EC2 Default User <ec2-user@ip-172-31-22-124.ec2.internal>
2025-06-02 19:21:52 +0000

    changes

--- homework.sh
+++ homework.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+function kpop() {
 sudo useradd jenny
 sudo useradd rose
 sudo useradd lisa
@@ -7,8 +10,9 @@ sudo groupadd blackpink
 sudo usermod -a -G blackpink jenny
 sudo usermod -a -G blackpink rose
 sudo usermod -a -G blackpink lisa
-sudo usermod -a -G blackpink jisoo
+sudo usermod -a -G blackpink jisoo}
 
+function wordpress(){
 sudo dnf install wget php-mysqlnd httpd php-fpm php-mysqli mariadb105-server php-json php php-devel -y
 wget https://wordpress.org/latest.tar.gz
 tar zxf latest.tar.gz
@@ -16,9 +20,10 @@ sudo mv wordpress/* /var/www/html
 sudo chown -R apache:apache /var/www/html
 sudo chmod -R 755 /var/www/html
 sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo systemctl enable httpd}
 
+function binary(){
 sudo yum install tree -y
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo yum -y install terraform}
